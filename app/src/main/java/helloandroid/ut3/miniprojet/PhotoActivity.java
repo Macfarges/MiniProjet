96e628877f6d0e34552441eb6c88ab3520d97320
Sylvain <sylvain.roehrig@gmail.com>
2024-01-18 12:51:17 +0100

    Add activities

---
+++ app/src/main/java/helloandroid/ut3/miniprojet/PhotoActivity.java
@@ -0,0 +1,14 @@
+package helloandroid.ut3.miniprojet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
