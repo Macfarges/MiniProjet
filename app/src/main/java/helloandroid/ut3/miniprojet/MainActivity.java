12a59a8b0ac026fdee32c691e7f3a8841a37e4c5
Matthias Farges <matthiias.farges@gmail.com>
2024-02-01 11:23:15 +0100

    Get restaurants

--- app/src/main/java/helloandroid/ut3/miniprojet/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/miniprojet/MainActivity.java
@@ -4,11 +4,32 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import helloandroid.ut3.miniprojet.data.domain.Restaurant;
+import helloandroid.ut3.miniprojet.data.service.FirebaseManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseManager.getInstance().getRestaurants(new FirebaseManager.DataCallback<List<Restaurant>>() {
+            @Override
+            public void onSuccess(List<Restaurant> restaurants) {
+                for (Restaurant restaurant : restaurants) {
+                    System.out.println(restaurant.getTitle());
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                // Handle the error
+            }
+        });
+//        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
     }
 }
